24afc5d5603a81d93ecda5647c588550cf2d4ed1
user23 <user23@pop-os.necromancer.net>
2025-12-10 21:26:49 +0100

    fix: test Test

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,18 +8,13 @@ from utils import runSync
 class TestMainFunctions(unittest.TestCase):
 
     @patch('utils.dumpSpotifyPlaylists')
-    @patch('utils.dumpPlexPlaylists')
-    @patch('utils.diffAndSyncPlaylists')
-    def test_runSync(self, mock_diff_and_sync, mock_dump_plex, mock_dump_spotify):
-        plex = MagicMock(spec=PlexServer)
+    def test_runSync(self, mock_dump_spotify):
         sp = MagicMock()
         spotify_uris = [{'user': 'username'}]
 
-        runSync(plex, sp, spotify_uris)
+        runSync(sp, spotify_uris)
 
         mock_dump_spotify.assert_called_once_with(sp, spotify_uris)
-        mock_dump_plex.assert_called_once_with(plex)
-        mock_diff_and_sync.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
